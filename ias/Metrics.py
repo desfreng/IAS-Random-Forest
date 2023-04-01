edc979f072a003cf3d9f0baf39148c46b6305fbb
Gabriel Desfrene <desfrene.gabriel@gmail.com>
2023-04-01 15:47:19 +0200

    Confusion Matrix Viewer done

--- ias/Metrics.py
+++ ias/Metrics.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -19,6 +20,33 @@ def confusion_matrix(nb_class: int, known_labels: np.ndarray,
     return conf_matrix
 
 
-def show_confusion_matrix(conf_mat: np.ndarray) -> None:
-    # Todo : Complete this Function
-    pass
+def show_confusion_matrix(conf_mat: np.ndarray, class_labels=None, ax=None):
+    text_kwargs = dict(ha='center', va='center', color='black')
+    nb_class = len(conf_mat)
+
+    if class_labels is None:
+        class_labels = np.arange(nb_class)
+
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.figure
+
+    img = ax.imshow(conf_mat)
+
+    for i in range(nb_class):
+        for j in range(nb_class):
+            ax.text(j, i, str(conf_mat[i, j]), **text_kwargs)
+
+    fig.colorbar(img, ax=ax)
+
+    ax.set(
+        xticks=np.arange(nb_class),
+        yticks=np.arange(nb_class),
+        xticklabels=class_labels,
+        yticklabels=class_labels,
+        ylabel="Known labels",
+        xlabel="Predicted labels",
+    )
+
+    return fig, ax
