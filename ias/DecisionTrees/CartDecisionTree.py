450974357e5286c25bb76de21aed54910fd6d12f
Gabriel Desfrene <desfrene.gabriel@gmail.com>
2023-04-04 14:51:16 +0200

    Changes by louismilhaud

--- ias/DecisionTrees/CartDecisionTree.py
+++ ias/DecisionTrees/CartDecisionTree.py
@@ -6,17 +6,21 @@ from ..utils import calculate_mean_criterion, criterion, shrunk_proba_vector, su
 
 
 class DecisionTree(AbstractDecisionTree):
-    def __init__(self, max_depth=np.inf, subset_size: int | None = None):
+    def __init__(self, max_depth=np.inf, subset_size: int | None = None,
+                 criterion_name: str = "gini"):
         super().__init__(max_depth, "gini")
         self._subset_size = subset_size
+        self._criterion_name = criterion_name
 
     def _find_threshold(self, data_set, label_set) -> tuple[criterion, int, float]:
         """
         Finds best threshold to split the dataset.
         The best (feature, threshold) is chosen between a random subset of y.
+        :param data_set: the dataset
+        :param label_set: the label_set
+        :return: tuple containing (the best criterion value, feature number, threshold value)
         """
-        # TODO : Check this method with refactoring...
-        best_gini = self.compute_criterion(label_set)
+        best_criterion = 2
 
         if self._subset_size is None:
             bag = subset_bagging(int(np.sqrt(self.features_number)), self.features_number)
@@ -24,7 +28,7 @@ class DecisionTree(AbstractDecisionTree):
             bag = subset_bagging(self._subset_size, self.features_number)
 
         res_f, res_t = bag[0], data_set[0][bag[0]]
-
+        r_x = np.empty(0)
         for f in bag:  # on itère sur les features d'un random subset
             l_x, r_x = np.empty(data_set.shape), data_set[data_set[:, f].argsort()]
             l_y, r_y = np.empty(label_set.shape), label_set[data_set[:, f].argsort()]
@@ -35,6 +39,8 @@ class DecisionTree(AbstractDecisionTree):
                 if new_gini < best_criterion:
                     best_criterion = new_gini
                     res_f, res_t = f, r_x[0][f]
+        if r_x.size == 0:
+            return self._find_threshold(data_set, label_set)
 
         return best_criterion, res_f, res_t
 
