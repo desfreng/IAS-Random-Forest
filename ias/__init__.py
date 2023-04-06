fded70a02388705c9de713168f0d6372934b157c
Gabriel Desfrene <desfrene.gabriel@gmail.com>
2023-04-06 15:37:55 +0200

    Added Test

--- ias/__init__.py
+++ ias/__init__.py
@@ -3,5 +3,6 @@ from . import Metrics
 from .DecisionTree import DecisionTree
 from .PCA import PCA
 from .RandomForest import RandomForest
+from .utils import split_dataset
 
-__all__ = ["Metrics", "Datasets", "PCA", "RandomForest", "DecisionTree"]
+__all__ = ["Metrics", "Datasets", "PCA", "RandomForest", "DecisionTree", "split_dataset"]
--- ias/utils.py
+++ ias/utils.py
@@ -1,11 +1,30 @@
-from typing import NewType
+from typing import NewType, Union
 
 import numpy as np
+
+from ias.Datasets import Dataset
 
 attributes = NewType("Array of attributes", np.ndarray[float])
 proba = NewType("Probability", float)
 class_id = NewType("Id of Classes", int)
 
+
+def split_dataset(ratio: Union[int, float], dataset: Dataset) \
+        -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    indices = np.arange(len(dataset.attributes))
+    np.random.shuffle(indices)
+    if isinstance(ratio, float):
+        train_size = int(len(dataset.attributes) * ratio)
+    elif isinstance(ratio, int):
+        train_size = ratio
+    else:
+        raise ValueError("Error")
+
+    return (dataset.attributes[indices[:train_size]], dataset.labels[indices[:train_size]],
+            dataset.attributes[indices[train_size:]], dataset.labels[indices[train_size:]])
+
+
+# ----------------------------------- PROBABILITY VECTORS -----------------------------------#
 
 def shrunk_proba_vector(label_list: np.ndarray) -> np.ndarray:
     """ Return the list of probabilities of each occurring class in a random order"""
@@ -24,18 +43,19 @@ def np_unique_to_proba_vector(np_unique_return, number_of_class: int) -> np.ndar
     return proba_vector / np.sum(class_distribution)
 
 
+def is_label_set_pure(label_set: np.ndarray[class_id]) -> bool:
+    return len(np.unique(label_set)) == 1
+
+
+# ----------------------------------- CRITERION COMPUTING -----------------------------------#
+
 def calculate_mean_criterion(l_y, r_y, index_calculator):
     """ Calculate gini index from two different subset """
     l, r = index_calculator(l_y), index_calculator(r_y)
     return (l_y.size * l + r_y.size * r) / (l_y.size + r_y.size)
 
 
-def is_label_set_pure(label_set: np.ndarray[class_id]) -> bool:
-    return len(np.unique(label_set)) == 1
-
-
 # ----------------------------------- GINI CRITERION -----------------------------------#
-
 def calculate_gini(y):
     """
     calculate gini index from array of class labels
