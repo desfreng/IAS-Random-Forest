correct variance and biais naive estimation

ias/Metrics.py
@@ -15,12 +15,13 @@ def accuracy_score(known_labels: np.ndarray, predicted_labels: np.ndarray) -> fl
 
 
 def estimate_variance(known_labels: np.ndarray, predicted_labels_train: np.ndarray, predicted_labels_test: np.ndarray) -> float:
-    neg_train = np.count_nonzero(known_labels != predicted_labels_train)
-    neg_test = np.count_nonzero(known_labels != predicted_labels_test)
     accuracy_train = accuracy_score(known_labels, predicted_labels_train)
     accuracy_test = accuracy_score(known_labels, predicted_labels_test)
     epsilon = 1e-7
-    return np.abs(neg_train/(accuracy_train+epsilon) - neg_test/(accuracy_test+epsilon))
+    return np.abs(accuracy_train - accuracy_test)
+
+def estimate_variance(known_labels: np.ndarray, predicted_train_label: np.ndarray) -> float:
+    return 1 - accuracy_score(known_labels, predicted_labels)
 
 
 def confusion_matrix(nb_class: int, known_labels: np.ndarray,